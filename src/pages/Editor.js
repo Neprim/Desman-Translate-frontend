91f5610fc05213934bb807d70d014b4b99ffbf03
Neprim <dlya.igr4444@yandex.ru>
2024-07-01 15:05:15 +0300

    Добавил автовнесение текста оригинальной строки в поле перевода

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -149,7 +149,7 @@ export default function Editor() {
 
 	useEffect(() => {
 		setTranslationEdit(null)
-		setInputTranslation(curString.text)
+		setInputTranslation(curString?.text || "")
 	}, [curStringIndex])
 
 	useEffect(() => {
