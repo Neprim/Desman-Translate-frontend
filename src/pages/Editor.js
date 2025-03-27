3cec4545fc254b53f987653733f44123e8b2eeb5
Neprim <dlya.igr4444@yandex.ru>
2025-03-27 19:54:55 +0300

    Добавил запрос подтверждения на добавление перевода без слова из словаря.

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -673,6 +673,11 @@ export default function Editor() {
 	}
 
 	async function AddTranslation() {
+		if (translateWarning) {
+			if (!window.confirm(getLoc("editor_are_you_sure_missing_dict_translation"))) {
+				return
+			}
+		}
 		setLoading(true)
 		try {
 			const sec = sections[curString.sec_ind]
